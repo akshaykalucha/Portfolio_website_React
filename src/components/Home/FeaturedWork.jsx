fa515818362a30b679a3391b844b0d1b8810fa40
akshay kalucha <akshaykalucha@gmail.com>
2020-05-27 13:09:51 +0530

    updates

--- src/components/Home/FeaturedWork.jsx
+++ src/components/Home/FeaturedWork.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import './featuredwork.css'
 
 function FeaturedWork() {
     return (
         <div>
-            <h1>Fearured work</h1>
+            <div className="container_card">
+                <h1 className="featured_heading">Featured work</h1>
+                <h3 className="featured_subheading">Side Projects</h3>
+                    <div className="card_container">
+                    <div className="featured__work">
+                        <div className="work__card">
+                            <div className="card__main-heading">
+                                <h1>
+                                    Catchit 1.0
+                                </h1>
+                                <div className="card-image">
+                                    <picture>
+                                        <img src="https://smakosh.com/static/2a6a2fe9035f56572ff11a4368086561/f4ab0/catchit.jpg" alt=""/>
+                                    </picture>
+                                </div>
+                                <div className="card__info-footer">
+                                    <p>Collect stars & avoid obstacles while trying to put the ball into the right basket to unlock new levels!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="work__card">
+                            <div className="card__main-heading">
+                                <h1>
+                                    Catchit 1.0
+                                </h1>
+                                <div className="card-image">
+                                    <picture>
+                                        <img src="https://smakosh.com/static/2a6a2fe9035f56572ff11a4368086561/f4ab0/catchit.jpg" alt=""/>
+                                    </picture>
+                                </div>
+                                <div className="card__info-footer">
+                                    <p>Collect stars & avoid obstacles while trying to put the ball into the right basket to unlock new levels!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="work__card">
+                            <div className="card__main-heading">
+                                <h1>
+                                    Catchit 1.0
+                                </h1>
+                                <div className="card-image">
+                                    <picture>
+                                        <img src="https://smakosh.com/static/2a6a2fe9035f56572ff11a4368086561/f4ab0/catchit.jpg" alt=""/>
+                                    </picture>
+                                </div>
+                                <div className="card__info-footer">
+                                    <p>Collect stars & avoid obstacles while trying to put the ball into the right basket to unlock new levels!</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
