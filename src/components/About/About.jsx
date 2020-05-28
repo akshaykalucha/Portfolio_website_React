102a0bdef7e3e5ff3791c7b911a0bd3076451054
akshay kalucha <akshaykalucha@gmail.com>
2020-05-29 01:56:29 +0530

    about desktop

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,8 +4,11 @@ import Footer from '../Base/Footer'
 import Backdrop from '../Base/Backdrop';
 import * as actionTypes from '../Store/actions'
 import { connect } from 'react-redux'
-
-
+import './about.css'
+import github from './github.svg'
+import linkedin from './linkedin.svg'
+import instagram from './instagram.svg'
+import twitter from './twitter.svg'
 
 
 function About(props) {
@@ -33,7 +36,48 @@ function About(props) {
             <div>
                 <Nav unshow={backDropClickHandler} show ={props.DrawerOpenStore} drawer={props.DrawerOpenStore} drawerClickHandler={drawerToggleClickHandler} />
                 {backDrop}
-                    <h1>This is about page</h1>
+                    <section className="about__section">
+                        <div className="about__container">
+                            <h1>About Me</h1>
+                            <p>I’m Akshay Kalucha, also known as Akcosh, I’m a self-taught software developer, Graphic, UI/UX Designer
+                               and full stack JavaScript developer. I'm really interested in Technologies & solving dev problems. 
+                               You can know more about me by reading my articles. Currently I'm working onsite for Intellify Delhi as a 
+                               Full stack software developer and i love to work on new technologies while exploring and learning the core 
+                               concepts of development.</p>
+                        </div>
+                        <div className="inquiry__container">
+                            <p className="inqueries__para">For any inquiries feel free to get in touch with me at:</p>
+                            <div className="contact-info">
+                                <i className="fas fa-envelope envelope"></i><p className="email">akshay23l@hotmail.com</p>
+                                <i className="fas fa-phone phone"></i><p className="phonenumber">************</p>
+                                <i className="fas fa-map-marker-alt marker"></i><p className="location">626 ***** ****, New Delhi</p>
+                                <i className="fas fa-map-marker-alt marker2"></i><p className="location2">Somewhere in North Campus, Delhi University, Delhi</p>
+                            </div>
+                        </div>
+                        <div className="web__inquiry">
+                            <h2 className="somewhere">Or somewhere else on the web</h2>
+                            <div className="web__inquiry__main">
+                                <a href="/">
+                                    <img src={twitter} alt=""/>
+                                    Twitter
+                                </a>
+                                <a href="/">
+                                    <img src={instagram} alt=""/>
+                                    Instagram
+                                </a>
+
+                                <a href="/">
+                                    <img className="github__inquiry-img" src={github} alt=""/>
+                                    GitHub
+                                </a>
+
+                                <a href="/">
+                                    <img className="linkedin__inquiry-img" src={linkedin} alt=""/>
+                                    Linkedin
+                                </a>
+                            </div>
+                        </div>
+                    </section>
                 <Footer />
             </div>
         )
