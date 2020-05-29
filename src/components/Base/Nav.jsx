e3661b83f837a0f137eaad89327275622998f983
akshay kalucha <akshaykalucha@gmail.com>
2020-05-29 19:32:32 +0530

    nav fuction

--- src/components/Base/Nav.jsx
+++ src/components/Base/Nav.jsx
@@ -1,15 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import NavDesktop from './NavDesktop'
 import NavMobile from './NavMobile';
 import SideDrawer from '../Base/SideDrawer';
 import { connect } from 'react-redux'
-import * as actionTypes from '../Store/actions'
+import * as actionTypes from '../Store/actions';
+import Backdrop from '../Base/Backdrop';
+
 
 
 
 
 function Nav(props) {
 
+
+    const drawerToggleClickHandler = () => {
+        props.setDrawer(!props.DrawerOpenStore)
+    }
+
+    const backDropClickHandler = () => {
+        props.setDrawer(false)
+
+    }
 
     function resize() {
         let currentHideNav = (window.innerWidth <= 768);
@@ -26,16 +37,26 @@ function Nav(props) {
         resize();
     })
 
+    let backDrop;
+
+    if(props.DrawerOpenStore){
+        backDrop = <Backdrop click ={backDropClickHandler} />
+    }
+
 
         if(props.isMobileStore===false){
             return(
-                <NavDesktop />
+                <div>
+                    <NavDesktop />
+                    {backDrop}
+                </div>
             )
         }else{
             return(
                 <div>
-                <NavMobile drawerHandler={props.drawer} click={props.drawerClickHandler} />
-                <SideDrawer unshowsideDrawer={props.unshow} showSidedrawer ={props.show}  />
+                <NavMobile drawerHandler={props.DrawerOpenStore} click={drawerToggleClickHandler} />
+                {backDrop}
+                <SideDrawer unshowsideDrawer={backDropClickHandler} showSidedrawer={props.DrawerOpenStore}  />
                 </div>
             )
         }
@@ -43,13 +64,15 @@ function Nav(props) {
 
     const mapStateToProps = state => {
         return {
-            isMobileStore: state.isMobile
+            isMobileStore: state.isMobile,
+            DrawerOpenStore: state.sideDrawerOpen,
         }
     }
     
     const mapDispatchToProps = dispatch => {
         return {
-            setIsMobile: (value) => dispatch({type: actionTypes.SET_MOBILE, param: value})
+            setIsMobile: (value) => dispatch({type: actionTypes.SET_MOBILE, param: value}),
+            setDrawer: (value) => dispatch({type: actionTypes.SHOW_DRAWER, param: value})
         }}
     
     export default connect(mapStateToProps, mapDispatchToProps)(Nav)
