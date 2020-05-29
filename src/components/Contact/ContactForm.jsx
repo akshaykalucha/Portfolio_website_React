364428fe42f7340b385428e0a83010074b4e16d8
akshay kalucha <akshaykalucha@gmail.com>
2020-05-30 00:38:10 +0530

    contact gorm

---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import Nav from '../Base/Nav'
+import Footer from '../Base/Footer'
+import './contactform.css'
+
+export class ContactForm extends Component {
+
+    state={
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Nav />
+                <div className="contactForm__main">
+                    <div className="contactForm">
+                        <h4 className="form-heading">
+                            Feel free to email me via
+                            <a className="my__email" href="akshay23l@hotmail.com" target="_top">akshay23l@hotmail.com"</a>
+                        </h4>
+                        <p className="form__para">Or fill in the contact form down below</p>
+                        <form className="form-main" action="">
+                            <div className="fullname">
+                                <label htmlFor="name" aria-label="please insert your name">
+                                    Funn name: <br/>
+                                    <input className="fillnameLabel" type="text" name="name" id="name" />
+                                </label>
+                                <div className="error__message__fullname"></div>
+                            </div>
+                            <div className="emai">
+                                Your email: <br/>
+                                <label htmlFor="email" aria-label="please insert your email">
+                                    <input className="emailLabel" type="text" name="email" id="email" />
+                                </label>
+                                <div className="error__message__email"></div>
+                            </div>
+                            <div className="message">
+                                <label htmlFor="name" aria-label="please insert your message">
+                                    Message: <br/>
+                                    <textarea className="messageLabel" type="text" name="message" id="message" />
+                                </label>
+                                <div className="error__message__message"></div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default ContactForm
--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Index from './Index'
 import About from '../About/About'
+import ContactForm from '../Contact/ContactForm'
 
 export default class Main extends Component {
     render() {
@@ -11,6 +12,7 @@ export default class Main extends Component {
                     <Switch>
                         <Route path="/" exact component={Index} />
                         <Route path="/about" exact component={About} />
+                        <Route path="/contact" exact component={ContactForm} />
                     </Switch>
                 </Router>
             </div>
