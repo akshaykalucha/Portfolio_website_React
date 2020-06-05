f8434d79f57a52deece807a5f88aa0f6e9a0d0cc
akshay kalucha <akshaykalucha@gmail.com>
2020-06-05 20:14:45 +0530

    updates

--- src/components/Home/BlogArticle.jsx
+++ src/components/Home/BlogArticle.jsx
@@ -121,30 +121,6 @@ function BlogArticle(props) {
             </div>
     )
 
-
-
-    // return (
-        // <div>
-        //     <div className="blogArticle__main">
-        //         <div className="main-component">
-        //             <div className="headings">
-        //                 <h1>Popular Articles</h1>
-        //             </div>
-        //             <div className="cards">
-        //                 <div className="thumbnail">
-        //                     <img src="" alt=""/>
-        //                 </div>
-        //                 <div className="card__heading">
-        //                     <h1>Card Heading</h1>
-        //                 </div>
-        //                 <div className="card__intro">
-        //                     <p></p>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </div>
-    // )
 }
 
 const mapStateToProps = state => {
