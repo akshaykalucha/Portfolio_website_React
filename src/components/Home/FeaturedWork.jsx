92c4ca9b7c0aa97f8bbe0007d85db59f81a60d4b
akshay kalucha <akshaykalucha@gmail.com>
2020-06-27 01:12:27 +0530

    css changes

--- src/components/Home/FeaturedWork.jsx
+++ src/components/Home/FeaturedWork.jsx
@@ -1,55 +1,60 @@
-import React, { useEffect, useState } from 'react'
+import React, {  } from 'react'
 import './featuredwork.css'
-import Aos from 'aos'
+// import Aos from 'aos'
 import 'aos/dist/aos.css'
 import { connect } from 'react-redux'
 
 
 
-let AOS1
-let AOS2
-let AOS3
-let anchorAOS
+// let AOS1
+// let AOS2
+// let AOS3
+// let anchorAOS
 
 
 function FeaturedWork(props) {
 
-    const [isMobile, setisMobile] = useState(false)
+    // const [isMobile, setisMobile] = useState(false)
     // console.log(isMobile)
 
 
-    if(isMobile===true){
-        AOS1 = "fade-up"
-        AOS2 = "fade-left"
-        AOS3 = "fade-right"
-        anchorAOS = null
+    // if(isMobile===true){
+    //     AOS1 = "fade-up"
+    //     AOS2 = "fade-left"
+    //     AOS3 = "fade-right"
+    //     anchorAOS = null
 
-    }else if(isMobile===false){
-        AOS1 = "fade-up"
-        AOS2 = "fade-up"
-        AOS3 = "fade-up"
-        anchorAOS = ".container_card"
+    // }else if(isMobile===false){
+    //     AOS1 = "fade-up"
+    //     AOS2 = "fade-up"
+    //     AOS3 = "fade-up"
+    //     anchorAOS = ".container_card"
+    // }
+
+    function scalezoom(e){
+        console.log("touched")
     }
 
 
-    function resize() {
-        let currentHideNav = (window.innerWidth <= 760);
-        if (currentHideNav) {
-            setisMobile(true);
-        }
-        else{
-            setisMobile(false)
-        }
-    }
+    // function resize() {
+    //     let currentHideNav = (window.innerWidth <= 760);
+    //     if (currentHideNav) {
+    //         setisMobile(true);
+    //     }
+    //     else{
+    //         setisMobile(false)
+    //     }
+    // }
 
-    useEffect(() => {
-        Aos.init({});
-    }, [])
+    // useEffect(() => {
+    //     Aos.init({});
+    // }, [])
 
-    useEffect(() => {
-        window.addEventListener("resize", resize.bind(this));
-        resize();
-    })
+    // useEffect(() => {
+    //     /* eslint-disable */
+    //     window.addEventListener("resize", resize.bind(this));
+    //     resize();
+    // })
     return (
         <div>
             <div style={ props.isDarknessStore ? {backgroundColor: "#212121"} : null } className="container_card">
@@ -57,7 +62,22 @@ function FeaturedWork(props) {
                 <h3 style={ props.isDarknessStore ? {color: "white"} : null } className="featured_subheading">Side Projects</h3>
                     <div className="card_container">
                     <div className="featured__work">
-                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } data-aos={AOS1} data-aos-once="true" data-aos-anchor={anchorAOS} data-aos-offset="400" data-aos-delay="50" className="work__card">
+                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } className="work__card">
+                            <div className="card__main-heading">
+                                <h1 style={ props.isDarknessStore ? {color: "white"} : null }>
+                                    Catchit 1.0
+                                </h1>
+                                <div onTouchStart={e=>scalezoom(e)} className="card-image">
+                                    <picture>
+                                        <img src="https://smakosh.com/static/2a6a2fe9035f56572ff11a4368086561/f4ab0/catchit.jpg" alt=""/>
+                                    </picture>
+                                </div>
+                                <div className="card__info-footer">
+                                    <p style={ props.isDarknessStore ? {color: "white"} : null }>Collect stars & avoid obstacles while trying to put the ball into the right basket to unlock new levels!</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } className="work__card">
                             <div className="card__main-heading">
                                 <h1 style={ props.isDarknessStore ? {color: "white"} : null }>
                                     Catchit 1.0
@@ -72,22 +92,7 @@ function FeaturedWork(props) {
                                 </div>
                             </div>
                         </div>
-                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } data-aos={AOS2} data-aos-once="true" data-aos-anchor={anchorAOS} data-aos-offset="400" data-aos-delay="50" className="work__card">
-                            <div className="card__main-heading">
-                                <h1 style={ props.isDarknessStore ? {color: "white"} : null }>
-                                    Catchit 1.0
-                                </h1>
-                                <div className="card-image">
-                                    <picture>
-                                        <img src="https://smakosh.com/static/2a6a2fe9035f56572ff11a4368086561/f4ab0/catchit.jpg" alt=""/>
-                                    </picture>
-                                </div>
-                                <div className="card__info-footer">
-                                    <p style={ props.isDarknessStore ? {color: "white"} : null }>Collect stars & avoid obstacles while trying to put the ball into the right basket to unlock new levels!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } data-aos={AOS3} data-aos-once="true" data-aos-anchor={anchorAOS} data-aos-offset="400" data-aos-delay="50" className="work__card">
+                        <div style={ props.isDarknessStore ? {backgroundColor: "#2b2a2a", boxShadow: "none"} : null } className="work__card">
                             <div className="card__main-heading">
                                 <h1 style={ props.isDarknessStore ? {color: "white"} : null }>
                                     Catchit 1.0
