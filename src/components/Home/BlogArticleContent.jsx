0bc0aae5b85052cc9ce04185aa1971bbfddfdca3
akshay kalucha <akshaykalucha@gmail.com>
2020-06-30 17:00:09 +0530

    updates

--- src/components/Home/BlogArticleContent.jsx
+++ src/components/Home/BlogArticleContent.jsx
@@ -7,20 +7,20 @@ export const cardData = [
         title: "Handling SEO in React apps",
         content: "I have landed successfully in Bali, rented a scooter and been enjoying my ride from my place to Dojo Bali where I’m currently writing this article. SEO definition Search engine optimization (SEO) is the process of increasing",
         tags: ["react", "gatsby", "dev"],
-        date: "Jun 13, 2019 5 min"
+        date: "Aug 13, 2019 5 min"
     },
     {
         thumbnail: DarkMode,
         title: "How To add dark mode easily with a custom React hook",
         content: "In this quick article, I will be implementing dark mode to a quick React app with a custom React hook, Before we start, let’s see how is this supposed to work in three easy steps: We will check first if the chosen theme",
         tags: ["dev"],
-        date: "Mar 22, 2019 2 min"
+        date: "Oct 22, 2019 2 min"
     },        
     {
         thumbnail: TravelPic,
         title: "My trip to Chicago & why I hate traveling",
         content: "I wanted to visit Naperville,IL 3 years ago and even told my friends about it, but 2 years after that moment, I didn’t really want to travel anywhere anymore because in my opinion, the Internet (even if it’s not decentralized) saves",
         tags: ["travel", "bio"],
-        date: "Dec 25, 2017 4 min"
+        date: "Apr 15, 2020 4 min"
     }
 ]
