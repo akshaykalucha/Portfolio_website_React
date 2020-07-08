6825c6dd4c8b695a8d0f372ae3e66f9fdd4aaf5a
akshay kalucha <akshaykalucha@gmail.com>
2020-07-08 21:43:59 +0530

    update

--- src/components/Blog/BlogContent.jsx
+++ src/components/Blog/BlogContent.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ReactQuill from 'react-quill'
-import './bubble.css';
+import './quill.bubble.css';
 import './BlogContent.css'
 import axios from 'axios';
 import Loading from '../Home/Loading'
